a7026c3169448c4ec762b446072e521c31125230
iuliaalda <aldaiulia07@yahoo.com>
2021-12-12 18:40:40 +0200

    cleaned code

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,15 +1,5 @@
-import com.rabbitmq.client.ConnectionFactory;
-
-import java.net.URISyntaxException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 
 public class Config {
-    public static String EXCHANGE_NAME = "actions_exchange";
-    public static String host = "localhost";
-    public static String QUEUE_NAME = "read_queue";
-    public static ConnectionFactory connectionFactory;
     public static String type_cerere="CERERE";
     public static String type_oferta="OFERTA";
-
 }
